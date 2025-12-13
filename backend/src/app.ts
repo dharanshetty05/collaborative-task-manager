6a7df6a54534170715fb06c74d3eaa13ec8b1039
dharanshetty05 <dharanshetty05@gmail.com>
2025-12-13 19:21:39 +0530

    Setup express server with typescript structure

---
+++ backend/src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ status: "ok"});
+});
+
+export default app;
