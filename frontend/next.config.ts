bec2b1f6af5f46eb04fd98d5e7332cce0767c3c2
dharanshetty05 <dharanshetty05@gmail.com>
2025-12-20 15:00:48 +0530

    Proxy API requests via Next.js rewrites to fix auth cookies

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+
+  async rewrites() {
+      return [
+        {
+          source: "/api/:path*",
+          destination: "https://taskflow-backend-w8ce.onrender.com/api/:path*"
+        },
+      ];
+  },
 };
 
 export default nextConfig;
